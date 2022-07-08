908e5eb26eb40f6c3e9ce5e6ec8ff7b0b78e9ea7
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-08 21:11:27 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,63 @@
+'''Arithmetic Operators'''
+# a=float(input("Enter the value of a:"))
+# b=float(input("Enter the value of b:"))
+# a,b=[int(x) for x in input('Enter the value of a and b:').split()]
+# print(f"The value of {a} + {b} is {a+b}")
+# print(f"The value of {a} - {b} is {a-b}")
+# print(f"The value of {a} / {b} is {a/b}")
+# print(f"The value of {a} % {b} is {a%b}")
+# print(f"The value of {a} ** {b} is {a**b}")
+# print(f"The value of {a} * {b} is {a*b}")
+
+'''Floor Division'''
+# c=4//2
+# print(c)
+# a=4.0//2
+# print(a)
+# b=5//2
+# print(b)
+# d=5.0//2
+# print(d)
+
+'''Assignment Operator'''
+# a=2;b=6
+# a=a+b
+# print(a)
+# a=a+a
+# print(a)
+
+# a=5
+# a+=3
+# print(a)  #a=a+3 a=5+3 a=8
+
+# b=9
+# b-=4
+# print(b)  #b=b-4 b=9-4 b=5
+
+# c=1
+# c*=5
+# print(c)  #c=c*5 c=1*5 c=5
+
+# d=6
+# d/=2
+# print(d)  #d=2/6 d=3.0
+
+# a=4
+# a**=4  
+# print(a)  #a=256
+
+# p=float(input('Enter The Value of P:'))
+# print(f"The Value of p is {p}")
+# p+=2
+# print("The Value of p after p+=2 is {}".format(p))
+# print(p)
+# print(f'The Value of P is {p}')
+# p-=3
+# print('The Value of p after p-=3 is {}'.format(p))
+# p*=5
+# print('The Value of p after p*=5 is {}'.format(p))
+# p**=2
+# print('The Value of p after p**=2 is {}'.format(p))
+# p/=2
+# print('The Value Of p after p/=2 is {}'.format(p))
+
